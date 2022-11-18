0e67548fe75e0b23be9b34229b80666715b36bfa
Stephen Oba <obastepheno@gmail.com>
2022-11-18 23:06:58 +0100

    Add function to print a binary tree and function to create a node in a binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a binary tree node
+ *
+ * @parent: Parent to new node
+ * @value: value for new node
+ *
+ * Return: New node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = new_node->right = NULL;
+
+	if (parent)
+	{
+		if (value > parent->n)
+		{
+			parent->right = new_node;
+		}
+		else
+		{
+			parent->left = new_node;
+		}
+	}
+	return (new_node);
+}
