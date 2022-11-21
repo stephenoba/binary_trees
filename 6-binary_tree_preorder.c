520f4d4570639f987f2f0e1aa4a0642f8077ba24
Stephen Oba <obastepheno@gmail.com>
2022-11-21 22:57:15 +0100

    refractor code

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,37 +8,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *temp;
-
 	if (tree && func)
 	{
 		func(tree->n);
-		if (tree->left)
-		{
-			binary_tree_preorder(tree->left, func);
-		}
-		else if (tree->right)
-		{
-			binary_tree_preorder(tree->right, func);
-		}
-		else
-		{
-			if (tree->parent)
-			{
-				if (tree->parent->left == tree)
-				{
-					temp = tree->parent->right;
-					binary_tree_preorder(temp, func);
-				}
-				else
-				{
-					temp = tree->parent->parent;
-					if (temp && temp->right != tree->parent)
-					{
-						binary_tree_preorder(temp->right, func);
-					}
-				}
-			}
-		}
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
