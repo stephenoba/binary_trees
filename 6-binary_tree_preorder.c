76c238fe0bcae28f6d1a50778feb19de21ef4618
Stephen Oba <obastepheno@gmail.com>
2022-11-21 22:27:31 +0100

    Add function to traverse a tree in pre order

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Traverse a binary tree in pre-order
+ *
+ * @tree: Tree
+ * @func: function to print tree
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t *temp;
+
+	if (tree)
+	{
+		func(tree->n);
+		if (tree->left)
+		{
+			binary_tree_preorder(tree->left, func);
+		}
+		else if (tree->right)
+		{
+			binary_tree_preorder(tree->right, func);
+		}
+		else
+		{
+			if (tree->parent)
+			{
+				if (tree->parent->left == tree)
+				{
+					temp = tree->parent->right;
+					binary_tree_preorder(temp, func);
+				}
+				else
+				{
+					temp = tree->parent->parent->right;
+					if (temp && temp != tree->parent)
+					{
+						binary_tree_preorder(temp, func);
+					}
+				}
+			}
+		}
+	}
+}
