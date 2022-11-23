8d67b65968c06bae002c4bff14e50f8f1ac7ceeb
Stephen Oba <obastepheno@gmail.com>
2022-11-23 14:50:36 +0100

    Add function to count nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count number of nodes in a tree/sub-tree
+ *
+ * @tree: Tree
+ * Return: Count of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
